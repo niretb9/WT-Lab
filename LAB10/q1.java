078bbb1cbb7a245c05b699d94fe35e27be1115a7
Niret Badgire <niretb9@gmail.com>
2023-04-03 13:25:17 +0530

    .

--- LAB10/q1.java
+++ LAB10/q1.java
@@ -1,13 +1,35 @@
+/*Write an interface Numbers with a method int process
+(int x, int y). Write a class Sum in which the process()
+method finds the sum of two numbers and returns an int
+value. Write another class Average in which the process()
+method finds the average of the two numbers and returns
+an int value. Implement an application class Demo where
+the functionality of above two classes are tested that
+exhibit run time polymorphism. */
+// Numbers interface
 package LAB10;
-public class q1{
+interface Numbers{
+    int process(int x, int y);
+}
+class Sum implements Numbers {
+    public int process(int x, int y) {
+        return x + y;
+    }
+}
+class Average implements Numbers {
+    public int process(int x, int y) {
+        return (x + y) / 2;
+    }
+}
+public class q1 {
     public static void main(String[] args) {
-        int[] array = new int[5];
-
-        try {
-            int value = array[5];
-            System.out.println("Value: " + value);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Caught an ArrayIndexOutOfBoundsException: " + e.getMessage());
-        }
+        Numbers sum = new Sum();
+        Numbers average = new Average();
+        int x = 10;
+        int y = 20;
+        int sumResult = sum.process(x, y);
+        System.out.println("Sum of " + x + " and " + y + " is " + sumResult);
+        int averageResult = average.process(x, y);
+        System.out.println("Average of " + x + " and " + y + " is " + averageResult);
     }
 }
---
+++ LAB10/q2.java
@@ -0,0 +1,29 @@
+package LAB10;
+import java.util.Scanner;
+class Circle {
+    private double radius;
+    public void set_radius(double radius) {
+        this.radius = radius;
+    }
+
+    public double get_radius(){
+        return radius;
+    }
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+public class q2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter radius of circle ");
+        double radius = sc.nextDouble();
+        Circle c = new Circle();
+        c.set_radius(radius);
+        System.out.println("area" + c.getArea());
+        System.out.println("preimeter is " + c.getPerimeter());
+    }
+}
